80c823307da6d0a500f1640baf980f5723b32e95
Juan Felipe Ortega <felipeortega383@gmail.com>
2023-02-28 10:21:36 -0500

    Fusion de menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileManu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileManu() {
+    mobileMenu.classList.toggle('inactive');
 }
 
